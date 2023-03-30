4e18130c284b76094b11a05c1ae546baabc240ac
GlaV1 <94520730+GlaV1@users.noreply.github.com>
2023-03-30 11:30:41 +0300

    Delete BiletRezervasyon directory

--- BiletRezervasyon/3/script.js
+++
@@ -1,9 +0,0 @@
-const container=document.querySelector('.container');
-const count=document.getElementById('.count');
-const amount = document.getElementById('amount');
-const select = document.getElementById('movie');
-const seats=document.querySelectorAll('.seat:not(.reserved)');
-
-/*if(){
-;
-}*/
